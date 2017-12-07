3507a973b6128641462f948115158fcd144d1710
Duerton <duerton@gmail.com>
2017-12-06 22:26:11 -0200

    modelo e configuracao hibernate

---
+++ TrabalhoDac/src/dao/teste.java
@@ -0,0 +1,26 @@
+package dao;
+
+import javax.persistence.EntityManager;
+
+import modelo.Aluno;
+
+public class teste {
+
+	
+	public static void main(String [] args) {
+		
+		Aluno aluno = new Aluno();
+		
+		aluno.setEmail("duerton@gmail.com");
+		aluno.setLogin("duerton");
+		aluno.setNome("Duerton");
+		aluno.setSenha("duerton");
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+
+		em.getTransaction().begin();
+		em.persist(aluno);
+		em.getTransaction().commit();
+		em.close();
+	}
+}
